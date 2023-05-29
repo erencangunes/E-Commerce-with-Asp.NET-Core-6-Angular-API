d6ce591aea10e33bc0361a1e8842d8c799f61060
Erencan Güneş Masaustu <erencangunes@outlook.com.tr>
2023-05-29 20:29:36 +0300

    #8

--- Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
+++ Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretAPI.Domain.Entities;
+using ETicaretAPI.Domain.Entities.Common;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -18,5 +19,28 @@ namespace ETicaretAPI.Persistence.Contexts
 		public DbSet<Order> Orders { get; set; }
 		public DbSet<Customer> Customers { get; set; }
 
-	}
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            // ChangeTracker DBContext'ten geliyor
+            // ChangeTracker : Entitiler üzerinde yapılan değişikliklerin yada yeni eklenen verinin yakalanmasını sağlayan prooertydir.
+            // Update operasyonlarında track edilen verileri yakalayıp elde etmemizi sağlar.
+
+            var datas = ChangeTracker.Entries<BaseEntity>();
+
+			foreach (var data in datas)
+			{
+
+				var result = data.State switch
+				{
+					EntityState.Added => data.Entity.CreateDate = DateTime.UtcNow,
+                    EntityState.Modified => data.Entity.UpdateDate = DateTime.UtcNow
+
+                };
+
+			}
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+    }
 }
--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -26,18 +26,18 @@ namespace ETicaretAPI.API.Controllers
         [HttpGet]
         public async Task Get()
         {
-            //await _productWriteRepository.AddRangeAsync(new()
-            //{
-            //    new() { Id = Guid.NewGuid(), Name = "Product 1", Price = 100, CreateDate = DateTime.UtcNow, Stock = 10 },
-            //    new() { Id = Guid.NewGuid(), Name = "Product 2", Price = 200, CreateDate = DateTime.UtcNow, Stock = 20 },
-            //    new() { Id = Guid.NewGuid(), Name = "Product 3", Price = 300, CreateDate = DateTime.UtcNow, Stock = 30 },
-            //});
+            await _productWriteRepository.AddRangeAsync(new()
+            {
+                new() { Id = Guid.NewGuid(), Name = "Product 10", Price = 100,  Stock = 10 },
+                //new() { Id = Guid.NewGuid(), Name = "Product 2", Price = 200, CreateDate = DateTime.UtcNow, Stock = 20 },
+                //new() { Id = Guid.NewGuid(), Name = "Product 3", Price = 300, CreateDate = DateTime.UtcNow, Stock = 30 },
+            });
 
-            //var count = await _productWriteRepository.SaveAsync();
+            var count = await _productWriteRepository.SaveAsync();
 
-            Product p = await _productReadRepository.GetByIdAsync("234fadb3-6878-4067-acfb-82c6ea3b68b4",false);
-            p.Name = "Ahmet";
-            await _productWriteRepository.SaveAsync();
+            //Product p = await _productReadRepository.GetByIdAsync("234fadb3-6878-4067-acfb-82c6ea3b68b4",false);
+            //p.Name = "Ahmet";
+            //await _productWriteRepository.SaveAsync();
         }
 
         [HttpGet("{id}")]
