d6b409201c8dfa1ba3e89965b8db57f9c4a22656
Erencan Güneş Masaustu <erencangunes@outlook.com.tr>
2023-05-29 19:59:26 +0300

    ..

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -1,14 +1,15 @@
 ﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
 using ETicaretAPI.Persistence.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ETicaretAPI.API.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class ProductsController : ControllerBase
-	{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
 
         readonly private IProductReadRepository _productReadRepository;
         readonly private IProductWriteRepository _productWriteRepository;
@@ -16,22 +17,34 @@ namespace ETicaretAPI.API.Controllers
         public ProductsController(
             IProductReadRepository productReadRepository,
             IProductWriteRepository productWriteRepository
-			)
+            )
         {
             _productReadRepository = productReadRepository;
-            _productWriteRepository = productWriteRepository;        
-		}
-
-		[HttpGet]
-		public async void Get()
-		{
-			await _productWriteRepository.AddRangeAsync(new()
-			{
-				new() { Id = Guid.NewGuid(), Name = "Product 1", Price = 100, CreateDate = DateTime.UtcNow, Stock = 10 },
-                new() { Id = Guid.NewGuid(), Name = "Product 2", Price = 200, CreateDate = DateTime.UtcNow, Stock = 20 },
-                new() { Id = Guid.NewGuid(), Name = "Product 3", Price = 300, CreateDate = DateTime.UtcNow, Stock = 30 },
-            });
-			var count = await _productWriteRepository.SaveAsync();
+            _productWriteRepository = productWriteRepository;
         }
-	}
+
+        [HttpGet]
+        public async Task Get()
+        {
+            //await _productWriteRepository.AddRangeAsync(new()
+            //{
+            //    new() { Id = Guid.NewGuid(), Name = "Product 1", Price = 100, CreateDate = DateTime.UtcNow, Stock = 10 },
+            //    new() { Id = Guid.NewGuid(), Name = "Product 2", Price = 200, CreateDate = DateTime.UtcNow, Stock = 20 },
+            //    new() { Id = Guid.NewGuid(), Name = "Product 3", Price = 300, CreateDate = DateTime.UtcNow, Stock = 30 },
+            //});
+
+            //var count = await _productWriteRepository.SaveAsync();
+
+            Product p = await _productReadRepository.GetByIdAsync("234fadb3-6878-4067-acfb-82c6ea3b68b4",false);
+            p.Name = "Ahmet";
+            await _productWriteRepository.SaveAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            Product product = await _productReadRepository.GetByIdAsync(id);
+            return Ok(product);
+        }
+    }
 }
